dcae89f2ab9b4a1a4a8897f75e113bf10f912ea2
njlewis <nj.lewis@yahoo.com>
2023-03-19 18:01:57 +0800

    Final 1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), tailwind()]
+});
 
 // git commit -m 'message'
 // git push
